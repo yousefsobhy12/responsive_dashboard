072ba0311cf1cb498b38dac0fae8e6cf131e771d
yousefsobhy12 <yousefsobhy124@gmail.com>
2024-07-08 03:40:58 +0300

    assets added

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,57 @@
+class Assets {
+  Assets._();
+
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar_1.svg
+  static const String imagesAvatar1 = "assets/images/avatar_1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar_2.svg
+  static const String imagesAvatar2 = "assets/images/avatar_2.svg";
+
+  /// Assets for imagesBalanca
+  /// assets/images/balanca.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCardBackground
+  /// assets/images/card_background.svg
+  static const String imagesCardBackground =
+      "assets/images/card_background.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMyInvestments
+  /// assets/images/my_investments.svg
+  static const String imagesMyInvestments = "assets/images/my_investments.svg";
+
+  /// Assets for imagesMyTransactions
+  /// assets/images/my_transactions.svg
+  static const String imagesMyTransactions =
+      "assets/images/my_transactions.svg";
+
+  /// Assets for imagesSettings
+  /// assets/images/settings.svg
+  static const String imagesSettings = "assets/images/settings.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesWalletAcc
+  /// assets/images/wallet_acc.svg
+  static const String imagesWalletAcc = "assets/images/wallet_acc.svg";
+}
