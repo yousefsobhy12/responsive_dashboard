8db64801a8db79380cfbbac6c34a07ad2865af44
yousefsobhy12 <yousefsobhy124@gmail.com>
2024-07-10 01:06:38 +0300

    custom background widget created

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_header.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_item_list_view.dart';
+import 'package:responsive_dash_board/widgets/custom_background_widget.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      child: const Column(
+    return const CustomBackgroundWidget(
+      child: Column(
         children: [
           AllExpensesHeader(),
           SizedBox(
@@ -27,3 +21,4 @@ class AllExpenses extends StatelessWidget {
     );
   }
 }
+
