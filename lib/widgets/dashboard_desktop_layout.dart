dadce676f2a44d68e67227d0f2763b9c1565b076
yousefsobhy12 <yousefsobhy124@gmail.com>
2024-07-16 01:32:14 +0300

    income section created and income section header done

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/my_card_and_transaction_history_section.dart';
+import 'package:responsive_dash_board/widgets/income_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -26,7 +26,7 @@ class DashboardDesktopLayout extends StatelessWidget {
         ),
         Expanded(
           flex: 2,
-          child: MyCardsAndTransactionHistorySection(),
+          child: IncomeSection(),
         ),
       ],
     );
