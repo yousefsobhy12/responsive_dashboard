e07cdfd76761a906a42c43588d5ec777ba78d723
yousefsobhy12 <yousefsobhy124@gmail.com>
2024-07-24 06:23:30 +0300

    device preview package added and mobile layout error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/dashboard_view.dart';
 
 void main() {
-  runApp(const ResponsiveDashboard());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const ResponsiveDashboard(),
+    ),
+  );
 }
 
 class ResponsiveDashboard extends StatelessWidget {
@@ -10,9 +16,11 @@ class ResponsiveDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DashboardView(),
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      home: const DashboardView(),
     );
   }
 }
--- lib/widgets/dashboard_mobile_layout.dart
+++ lib/widgets/dashboard_mobile_layout.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
-import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/income_chart.dart';
+import 'package:responsive_dash_board/widgets/income_section_details.dart';
+import 'package:responsive_dash_board/widgets/income_section_header.dart';
 import 'package:responsive_dash_board/widgets/my_card_and_transaction_history_section.dart';
 
 class DashboardMobileLayout extends StatelessWidget {
@@ -21,7 +23,24 @@ class DashboardMobileLayout extends StatelessWidget {
           SizedBox(
             height: 24,
           ),
-          IncomeSection(),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 12,
+            ),
+            child: Column(
+              children: [
+                IncomeSectionHeader(),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: IncomeChart(),
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                IncomeSectionDetails()
+              ],
+            ),
+          )
         ],
       ),
     );
